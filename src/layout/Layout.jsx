bf272e6735bedaef4db5844ca60e92ae688a2385
inforhustle-hash <inforhustle@gmail.com>
2025-12-13 13:43:02 -0700

    Update Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,20 @@
+import Sidebar from "./Sidebar";
+import Header from "./Header";
 import { Outlet } from "react-router-dom";
+import "./neon-theme.css";
 
 export default function Layout() {
   return (
-    <div style={{ display: "flex", minHeight: "100vh" }}>
-      <Outlet />
+    <div className="layout-container">
+      <Sidebar />
+
+      <div className="layout-main">
+        <Header />
+        <div className="layout-content">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
+
