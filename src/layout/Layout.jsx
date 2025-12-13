f487626de032ecc04d05ed1f54cf1d8262777ffc
inforhustle-hash <inforhustle@gmail.com>
2025-12-12 21:16:38 -0700

    Update Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,5 +1,9 @@
-import { Outlet, Link } from 'react-router-dom';
-import './neon-theme.css';
-import './sidebar.css';
-import './header.css';
-export default function Layout(){return(<div className='layout-container'><aside className='sidebar'><h2 className='logo'>HustleHub Funnels</h2><nav><Link to='/'>Dashboard</Link><Link to='/contacts'>Contacts</Link><Link to='/pipelines'>Pipelines</Link></nav></aside><main className='main-area'><header className='header'>HustleHub Funnels</header><div className='page-content'><Outlet /></div></main></div>);} 
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <div style={{ display: "flex", minHeight: "100vh" }}>
+      <Outlet />
+    </div>
+  );
+}
