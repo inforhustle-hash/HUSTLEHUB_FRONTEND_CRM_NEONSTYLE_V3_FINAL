059c2d6b0138250e41ee39ffa2ee1206d345eb37
inforhustle-hash <inforhustle@gmail.com>
2025-12-14 07:46:39 -0700

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -4,10 +4,11 @@ const API = axios.create({
   baseURL: "https://hustlehub-backend-crm-neonstyle-v3-final.onrender.com/api",
 });
 
+// Attach token automatically (if logged in)
 API.interceptors.request.use(config => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  const user = JSON.parse(localStorage.getItem("hustlehub_user"));
+  if (user?.token) {
+    config.headers.Authorization = `Bearer ${user.token}`;
   }
   return config;
 });
