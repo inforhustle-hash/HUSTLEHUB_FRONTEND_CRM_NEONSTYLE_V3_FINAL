9e12ab06c95985d756841088bfc71752fba1d031
inforhustle-hash <inforhustle@gmail.com>
2025-12-14 07:45:30 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppRouter from './router/Router.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import AppRouter from "./router/Router.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <AuthProvider>
       <AppRouter />
     </AuthProvider>
-  </BrowserRouter>
+  </React.StrictMode>
 );
